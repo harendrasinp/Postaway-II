57de893bfbc49e4f2c205b7c91a318db9db3f9d9
harendrasinhp <haren111990@gmail.com>
2025-01-14 13:04:32 +0530

    add server comments

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 import server from "./index.js";
 import MongoDbConnection from "./src/Database_Config/mongoose.connection.js";
-
+// --------------------------server -comment-------------------------------
 server.listen(8000,(req,res)=>{
     MongoDbConnection();
     console.log("Server is running on port 8000...");
